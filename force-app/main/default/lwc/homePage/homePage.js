11b19721765e05ae41880364c74d13389a97c0c4
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2025-02-27 14:01:13 -0600

    Implemented button navigation to claims page successfully

--- force-app/main/default/lwc/homePage/homePage.js
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -1,6 +1,17 @@
 import { LightningElement } from 'lwc';
 import bgImage from '@salesforce/resourceUrl/WelcomeBg';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class ESC_Homepage extends LightningElement {
+export default class ESC_Homepage extends NavigationMixin(LightningElement) {
   backgroundStyle = `background-image: url(${bgImage}); background-size: cover; background-position: center; width: 100%; padding: 100px 0;`;
+
+  navigateToClaims() {
+    console.log("Navigating to the page..");
+    this[NavigationMixin.Navigate]({
+        type: 'standard__webPage',
+        attributes: {
+            url: '/claims'
+        }
+    });
+  }
 }
